32433a83773d52d36b760291fd5e815d56586f67
Xiaohang0316 <84364287+Xiaohang0316@users.noreply.github.com>
2022-07-31 23:57:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Department />
+    <Postdepartment/>
+  </div>
+</template>
+
+<script>
+import Department from './components/department.vue'
+import Postdepartment from './components/postdepartment.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Department,
+    Postdepartment
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="hello">
+  <van-cell is-link title="基础用法" @click="show = true" />
+  <van-action-sheet v-model="show" :actions="actions" @select="onSelect" />
+</div>
+</template>
+
+<script>
+import {
+  Toast
+} from 'vant';
+export default {
+  name: 'Department',
+  data() {
+    return {
+      show: false,
+      actions: [{
+        name: '选项一'
+      }, {
+        name: '选项二'
+      }, {
+        name: '选项三'
+      }],
+    };
+  },
+  methods: {
+    onSelect(item) {
+      // 默认情况下点击选项时不会自动收起
+      // 可以通过 close-on-click-action 属性开启自动收起
+      this.show = false;
+      Toast(item.name);
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.hello {
+  width: 300px;
+
+}
+</style>
